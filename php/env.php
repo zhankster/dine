12b276eecb87aff9792a933d42eb784112f825c9
Hank Allen <zrefugee@gmail.com>
2018-06-20 20:44:04 -0500

    Add files via upload

---
+++ php/env.php
@@ -0,0 +1,16 @@
+<?php
+//Location Info
+$GLOBALS['loc_name'] = 'ZHankster\'s Dine In and Out';
+$GLOBALS['street_address'] = '202 Robertson Avenue';
+$GLOBALS['city'] =  'Rainsville';
+$GLOBALS['state'] = 'AL';
+$GLOBALS['zip'] = '35986';
+
+//Database 
+$GLOBALS['db_host'] = '127.0.0.1';
+$GLOBALS['db_name'] = 'dine';
+$GLOBALS['db_user'] = 'hda';
+$GLOBALS['db_pass'] = 'zhank2001';
+
+
+?>
